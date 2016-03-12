1d0f8963817ded719e84d61a34db8fa2f8c726de
ManuelLR <ManuelLR@github.com>
2016-03-12 20:13:38 +0100

    Test Populate

---
+++ WorkSpace/Acme-Six-Pack/src/test/java/domain/PopulateTest.java
@@ -0,0 +1,28 @@
+package domain;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import utilities.AbstractTest;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+	"classpath:spring/datasource.xml",
+	"classpath:spring/config/packages.xml"})
+@Transactional
+@TransactionConfiguration(defaultRollback = false)
+public class PopulateTest extends AbstractTest{
+
+	// Service under test -------------------------
+
+	
+	// Test ---------------------------------------
+	@Test
+	public void testCreate1(){
+		System.out.println("Test Populate");
+	}
+}
