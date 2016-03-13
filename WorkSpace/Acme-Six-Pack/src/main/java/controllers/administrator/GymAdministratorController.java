f90ee4cf911286ce5cee6c7e18ae16cb18d590f1
migrodcab <miguel.rc95@gmail.com>
2016-03-13 12:35:14 +0100

    pequeños cambios

--- WorkSpace/Acme-Six-Pack/src/main/java/controllers/administrator/GymAdministratorController.java
+++ WorkSpace/Acme-Six-Pack/src/main/java/controllers/administrator/GymAdministratorController.java
@@ -70,9 +70,9 @@ public class GymAdministratorController extends AbstractController {
 		result.addObject("requestURI", "gym/administrator/list.do?");
 		result.addObject("gyms", gyms);
 		result.addObject("customers", customers);
-		result.addObject("requestUri2", "service/list.do?");
-		result.addObject("requestUri3", "room/list.do?");
-		result.addObject("requestUri4", "bulletin/list.do?");
+		result.addObject("requestUri2", "service/administrator/list.do?");
+		result.addObject("requestUri3", "room/administrator/list.do?");
+		result.addObject("requestUri4", "bulletin/administrator/list.do?");
 
 		return result;
 	}
