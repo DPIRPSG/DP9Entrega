63250679c0d8a1e157e22b95f704eba0bf6585a1
migrodcab <miguel.rc95@gmail.com>
2016-03-24 19:25:14 +0100

    CurriculumServiceTestOk

--- WorkSpace/Acme-Six-Pack/src/test/java/services/CurriculumServiceTest.java
+++ WorkSpace/Acme-Six-Pack/src/test/java/services/CurriculumServiceTest.java
@@ -1,10 +1,14 @@
 package services;
 
 import java.util.Collection;
+import java.util.Date;
+
+import javax.validation.ConstraintViolationException;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
@@ -76,8 +80,182 @@ public class CurriculumServiceTest extends AbstractTest {
 		authenticate(null);
 	}
 	
-	@Test
+	/**
+	 * Test que crea un curriculum sin statement
+	 */
+	@Test(expected = ConstraintViolationException.class)
+	@Rollback(value=true)
 	public void testCreateCurriculumError1() {
+		Curriculum curriculum;
+		Trainer trainer;
+		Collection<Trainer> trainers;
+		String skills, likes, dislikes;
+		
+		trainers = trainerService.findAll();
+		trainer = null;
+		
+		skills = "Prueba";
+		likes = "Prueba";
+		dislikes = "Prueba";
+		
+		for(Trainer trainerAux : trainers) {
+			if(trainerAux.getCurriculum() == null) {
+				trainer = trainerAux;
+				break;
+			}
+		}
+		
+		authenticate(trainer.getUserAccount().getUsername());
+		
+		curriculum = curriculumService.create();
+		//curriculum.setStatement("Hola");
+		curriculum.setLikes(likes);
+		curriculum.setSkills(skills);
+		curriculum.setDislikes(dislikes);
+		curriculum = curriculumService.save(curriculum);
+		
+		Assert.isTrue(curriculum.getStatement().equals("Hola"));
+		Assert.isTrue(curriculum.getSkills().equals("Prueba"));
+		Assert.isTrue(curriculum.getLikes().equals("Prueba"));
+		Assert.isTrue(curriculum.getDislikes().equals("Prueba"));
+		Assert.isTrue(curriculum.getPicture() == trainer.getPicture());
+		
+		authenticate(null);
+	}
+	
+	/**
+	 * Test que crea un curriculum sin skills
+	 */
+	@Test(expected = ConstraintViolationException.class)
+	@Rollback(value=true)
+	public void testCreateCurriculumError2() {
+		Curriculum curriculum;
+		Trainer trainer;
+		Collection<Trainer> trainers;
+		String skills, likes, dislikes;
+		
+		trainers = trainerService.findAll();
+		trainer = null;
+		
+		skills = "Prueba";
+		likes = "Prueba";
+		dislikes = "Prueba";
+		
+		for(Trainer trainerAux : trainers) {
+			if(trainerAux.getCurriculum() == null) {
+				trainer = trainerAux;
+				break;
+			}
+		}
+		
+		authenticate(trainer.getUserAccount().getUsername());
+		
+		curriculum = curriculumService.create();
+		curriculum.setStatement("Hola");
+		curriculum.setLikes(likes);
+		//curriculum.setSkills(skills);
+		curriculum.setDislikes(dislikes);
+		curriculum = curriculumService.save(curriculum);
+		
+		Assert.isTrue(curriculum.getStatement().equals("Hola"));
+		Assert.isTrue(curriculum.getSkills().equals("Prueba"));
+		Assert.isTrue(curriculum.getLikes().equals("Prueba"));
+		Assert.isTrue(curriculum.getDislikes().equals("Prueba"));
+		Assert.isTrue(curriculum.getPicture() == trainer.getPicture());
+		
+		authenticate(null);
+	}
+	
+	/**
+	 * Test que crea un curriculum sin likes
+	 */
+	@Test(expected = ConstraintViolationException.class)
+	@Rollback(value=true)
+	public void testCreateCurriculumError3() {
+		Curriculum curriculum;
+		Trainer trainer;
+		Collection<Trainer> trainers;
+		String skills, likes, dislikes;
+		
+		trainers = trainerService.findAll();
+		trainer = null;
+		
+		skills = "Prueba";
+		likes = "Prueba";
+		dislikes = "Prueba";
+		
+		for(Trainer trainerAux : trainers) {
+			if(trainerAux.getCurriculum() == null) {
+				trainer = trainerAux;
+				break;
+			}
+		}
+		
+		authenticate(trainer.getUserAccount().getUsername());
+		
+		curriculum = curriculumService.create();
+		curriculum.setStatement("Hola");
+		//curriculum.setLikes(likes);
+		curriculum.setSkills(skills);
+		curriculum.setDislikes(dislikes);
+		curriculum = curriculumService.save(curriculum);
+		
+		Assert.isTrue(curriculum.getStatement().equals("Hola"));
+		Assert.isTrue(curriculum.getSkills().equals("Prueba"));
+		Assert.isTrue(curriculum.getLikes().equals("Prueba"));
+		Assert.isTrue(curriculum.getDislikes().equals("Prueba"));
+		Assert.isTrue(curriculum.getPicture() == trainer.getPicture());
+		
+		authenticate(null);
+	}
+	/**
+	 * Test que crea un curriculum sin dislikes
+	 */
+	@Test(expected = ConstraintViolationException.class)
+	@Rollback(value=true)
+	public void testCreateCurriculumError4() {
+		Curriculum curriculum;
+		Trainer trainer;
+		Collection<Trainer> trainers;
+		String skills, likes, dislikes;
+		
+		trainers = trainerService.findAll();
+		trainer = null;
+		
+		skills = "Prueba";
+		likes = "Prueba";
+		dislikes = "Prueba";
+		
+		for(Trainer trainerAux : trainers) {
+			if(trainerAux.getCurriculum() == null) {
+				trainer = trainerAux;
+				break;
+			}
+		}
+		
+		authenticate(trainer.getUserAccount().getUsername());
+		
+		curriculum = curriculumService.create();
+		curriculum.setStatement("Hola");
+		curriculum.setLikes(likes);
+		curriculum.setSkills(skills);
+		//curriculum.setDislikes(dislikes);
+		curriculum = curriculumService.save(curriculum);
+		
+		Assert.isTrue(curriculum.getStatement().equals("Hola"));
+		Assert.isTrue(curriculum.getSkills().equals("Prueba"));
+		Assert.isTrue(curriculum.getLikes().equals("Prueba"));
+		Assert.isTrue(curriculum.getDislikes().equals("Prueba"));
+		Assert.isTrue(curriculum.getPicture() == trainer.getPicture());
+		
+		authenticate(null);
+	}
+	
+	/**
+	 * Test que comprueba que al crear un curriculum sin foto, usa la del profile del Trainer
+	 */
+	@Test
+	public void testCreateCurriculumWithoutPicture() {
 		Curriculum curriculum;
 		Trainer trainer;
 		String skills, likes, dislikes;
@@ -101,9 +279,43 @@ public class CurriculumServiceTest extends AbstractTest {
 		Assert.isTrue(curriculum.getSkills().equals("Prueba"));
 		Assert.isTrue(curriculum.getLikes().equals("Prueba"));
 		Assert.isTrue(curriculum.getDislikes().equals("Prueba"));
-		System.out.println(curriculum.getPicture());
 		
 		Assert.isTrue(curriculum.getPicture().equals(trainer.getPicture()));
+		
+		authenticate(null);
+	}
+	@Test
+	public void testEditCurriculum() {
+		Trainer trainer;
+		Curriculum curriculum;
+		Date moment;
+		String skills, likes, dislikes;
+		
+		skills = "Prueba";
+		likes = "Prueba";
+		dislikes = "Prueba";
+			
+		authenticate("trainer1");
+		
+		trainer = trainerService.findByPrincipal();
+		curriculum = trainer.getCurriculum();
+		
+		moment = curriculum.getUpdateMoment();
+		
+		curriculum.setStatement("Hola");
+		curriculum.setLikes(likes);
+		curriculum.setSkills(skills);
+		curriculum.setDislikes(dislikes);
+		curriculum = curriculumService.save(curriculum);
+		
+		Assert.isTrue(curriculum.getStatement().equals("Hola"));
+		Assert.isTrue(curriculum.getSkills().equals("Prueba"));
+		Assert.isTrue(curriculum.getLikes().equals("Prueba"));
+		Assert.isTrue(curriculum.getDislikes().equals("Prueba"));
+		Assert.isTrue(curriculum.getPicture().equals(trainer.getPicture()));
+		
+		//Comprueba que cambia la fecha de actualizacion
+		Assert.isTrue(moment.before(curriculum.getUpdateMoment()));
 		
 		authenticate(null);
 	}
