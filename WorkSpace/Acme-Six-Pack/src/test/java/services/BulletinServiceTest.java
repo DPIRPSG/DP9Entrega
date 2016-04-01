25693c210c315a5dca786b09559d660731e563ef
guillermo-ag-95 <guillermo.ag.95@gmail.com>
2016-04-01 22:36:02 +0200

    Test básicos de Bulletin terminados
    - Things TO DO actualizado.
    - Test básicos de bulletin terminados.

--- WorkSpace/Acme-Six-Pack/src/test/java/services/BulletinServiceTest.java
+++ WorkSpace/Acme-Six-Pack/src/test/java/services/BulletinServiceTest.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
@@ -232,5 +233,140 @@ public class BulletinServiceTest extends AbstractTest{
 		
 		bulletinService.flush();
 		
+	}
+	
+	/**
+	 * Description: A user who is authenticated as an administrator must be able to manage the bulletins of a gym, which includes listing and creating them. Bulletins cannot be modified or deleted. Before publishing a bulletin, the system must ask for confirmation.
+	 * Precondition: The user is an admin. The selected gym must be a gym of the system.
+	 * Return: TRUE
+	 * Postcondition: All bulletin of the selected gym are shown.
+	 */
+	@Test
+	public void testFindAllBulletinsByGymAdmin1(){
+		
+		Collection<Bulletin> all;
+		Collection<Gym> allGyms;
+		Gym gym = null;
+		Double counter = 1.0;
+		
+		authenticate("admin");
+		
+		allGyms = gymService.findAll();
+		
+		for(Gym i: allGyms){
+			if(i.getName().equals("Gym Sevilla")){
+				gym = i;
+				counter = 0.0;
+				break;
+			}
+		}
+		
+		Assert.isTrue(counter == 0.0, "There wasn't any gym named as specified in the test");
+		Assert.notNull(gym);
+		
+		all = bulletinService.findAllByGymId(gym.getId());
+		
+		Assert.isTrue(all.size() == 3);
+		
+		authenticate(null);
+		bulletinService.flush();
+		
+	}
+	
+	/**
+	 * Description: A user who is authenticated as an administrator must be able to manage the bulletins of a gym, which includes listing and creating them. Bulletins cannot be modified or deleted. Before publishing a bulletin, the system must ask for confirmation.
+	 * Precondition: The user is an admin. The selected gym must not be a gym of the system.
+	 * Return: FALSE
+	 * Postcondition: - 
+	 */
+	@Test
+	public void testFindAllBulletinsByGymAdmin2(){
+		
+		Collection<Bulletin> all;
+		
+		all = bulletinService.findAllByGymId(1000);
+		
+		Assert.isTrue(all.size() == 0);
+		
+		bulletinService.flush();
+		
+	}
+	
+	/**
+	 * Description: A user who is authenticated as an administrator must be able to manage the bulletins of a gym, which includes listing and creating them. Bulletins cannot be modified or deleted. Before publishing a bulletin, the system must ask for confirmation.
+	 * Precondition: The user is an admin. The data of the new bulletin must be valid. The selected gym must be a gym of the system.
+	 * Return: TRUE
+	 * Postcondition: A new bulletin is created.
+	 */
+	@Test
+	public void testCreateBulletin1(){
+		
+		Collection<Bulletin> all;
+		Bulletin newBulletin = null;
+		Collection<Gym> allGyms;
+		Gym gym = null;
+		
+		authenticate("admin");
+		
+		allGyms = gymService.findAll();
+		
+		for(Gym i:allGyms){
+			if(i.getName().equals("Gym Sevilla")){
+				gym = i;
+				break;
+			}
+		}
+		
+		all = bulletinService.findAllByGymId(gym.getId());
+		Assert.isTrue(all.size() == 3);
+		
+		newBulletin = bulletinService.create(gym.getId());
+		newBulletin.setTitle("Nuevo bulletin");
+		newBulletin.setDescription("Este es un nuevo bulletin");
+		bulletinService.save(newBulletin);
+		
+		all = bulletinService.findAllByGymId(gym.getId());
+		Assert.isTrue(all.size() == 4);
+		
+		authenticate(null);
+		bulletinService.flush();
+	}
+	
+	/**
+	 * Description: A user who is authenticated as an administrator must be able to manage the bulletins of a gym, which includes listing and creating them. Bulletins cannot be modified or deleted. Before publishing a bulletin, the system must ask for confirmation.
+	 * Precondition: The user is not an admin. The data of the new room must be valid. The selected gym must be a gym of the system.
+	 * Return: FALSE
+	 * Postcondition: -
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	@Rollback(value = true)
+	public void testCreateBulletin2(){
+	
+		Collection<Bulletin> all;
+		Bulletin newBulletin = null;
+		Collection<Gym> allGyms;
+		Gym gym = null;
+				
+		allGyms = gymService.findAll();
+		
+		for(Gym i:allGyms){
+			if(i.getName().equals("Gym Sevilla")){
+				gym = i;
+				break;
+			}
+		}
+		
+		all = bulletinService.findAllByGymId(gym.getId());
+		Assert.isTrue(all.size() == 3);
+		
+		newBulletin = bulletinService.create(gym.getId());
+		newBulletin.setTitle("Nuevo bulletin");
+		newBulletin.setDescription("Este es un nuevo bulletin");
+		bulletinService.save(newBulletin);
+		
+		all = bulletinService.findAllByGymId(gym.getId());
+		Assert.isTrue(all.size() == 4);
+		
+		bulletinService.flush();
 	}
 }
