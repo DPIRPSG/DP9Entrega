8774d2b1c148c323689e3f6731ebe84ebe0de152
ManuelLR <ManuelLR@github.com>
2016-04-02 13:49:43 +0200

    Adding more tests

--- WorkSpace/Acme-Six-Pack/src/test/java/services/ActivityTest.java
+++ WorkSpace/Acme-Six-Pack/src/test/java/services/ActivityTest.java
@@ -1,5 +1,6 @@
 package services;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
 import java.util.Iterator;
@@ -22,6 +23,9 @@ import repositories.CustomerRepository;
 import domain.Activity;
 import domain.Customer;
 import domain.Gym;
+import domain.Room;
+import domain.ServiceEntity;
+import domain.Trainer;
 
 import utilities.AbstractTest;
 import utilities.InvalidPostTestException;
@@ -49,10 +53,16 @@ public class ActivityTest extends AbstractTest{
 	private ActivityService activityService;
 	
 	@Autowired
+	private ServiceService serviceService;
+	
+	@Autowired
 	private GymService gymService;
 	
 	@Autowired
 	private CustomerService customerService;
+	
+	@Autowired
+	private TrainerService trainerService;
 	
 	@Autowired
 	private CustomerRepository customerRepository;
@@ -65,11 +75,6 @@ public class ActivityTest extends AbstractTest{
 	 * Book an activity in a gym as long as he or she has paid the corresponding fee, there
 	 * is at least one seat available, and he or she has not booked any overlapping activities.
 	 * 
-	 * Caso positivo, todo ok
-	 * 		 + Seleccionar customer
-	 * 		 + Coger salas (de un gimnasio con pago activo) y seleccionar un activity (comprobar asientos libres, comprobar no overlapping, comprobar gimnasio con pago activo)
-	 * 		 + Añadir el customer a la activity
-	 * 		 + Comprobar que, efectivamente está añadido y no coincide
 	 */
 	@Test
 	public void testBookActivityOk(){
@@ -497,6 +502,7 @@ public class ActivityTest extends AbstractTest{
 		}
 		
 	}	
+	
 	@Test(expected=IllegalArgumentException.class)
 	@Transactional(noRollbackFor=IllegalArgumentException.class)
 	@Rollback(value=true)
@@ -560,6 +566,631 @@ public class ActivityTest extends AbstractTest{
 
 	}
 	
+
+	/**
+	 * Acme-Six-Pack-2.0 - Level B - 8.2
+	 * List the activities that he or she has booked
+	 * 
+	 */
+	@Test
+	public void testListActivitiesBookedCustoOk(){
+		Assert.notNull(null, "Método testListActivitiesBookedCustoOk no realizado");
+		Activity activity;
+		
+		// Load objects to test
+		
+		activity = null;
+		
+		// Checks basic requirements
+		try{
+			Assert.notNull(activity, "No se ha encontrado una actividad con la que realizar la comprobación");
+		}catch (Exception e) {
+			throw new InvalidPreTestException(e.toString());
+		}
+		
+		// Execution of test
+
+		// Checks results 
+
+	}
+	
+	/**
+	 * Acme-Six-Pack-2.0 - Level B - 8.3
+	 * Cancel a booking of his or her
+	 * 
+	 */
+	@Test
+	public void testCancelActivityBookedOk(){
+		Activity activity;
+		
+		// Load objects to test
+		authenticate("customer1");
+		activity = null;
+		
+		for(Activity a:activityService.findAllByCustomer()){
+			boolean isAcceptable;
+
+			isAcceptable = a.getStartingMoment().after(new Date()) && ! a.getDeleted();
+			
+			if(isAcceptable){
+				activity = a;
+				break;
+			}
+		}
+		
+		// Checks basic requirements
+		try{
+			Assert.notNull(activity, "No se ha encontrado una actividad con la que realizar la comprobación");
+		}catch (Exception e) {
+			throw new InvalidPreTestException(e.toString());
+		}
+		
+		// Execution of test
+		
+		authenticate("customer1");
+
+		activityService.cancel(activity);
+
+		// Checks results 
+		
+		Assert.isTrue(!activityService.findAllByCustomer().contains(activity), "La actividad no ha sido eliminada");
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	@Transactional(noRollbackFor=IllegalArgumentException.class)
+	@Rollback(value=true)
+	public void testCancelActivityBookedErrorNotAsigned(){
+		Activity activity;
+		
+		// Load objects to test
+		authenticate("customer1");
+		activity = null;
+		
+		for(Activity a:activityService.findAll()){
+			boolean isAcceptable;
+
+			isAcceptable = a.getStartingMoment().after(new Date()) && ! a.getDeleted();
+			
+			isAcceptable = isAcceptable && ! activityService.findAllByCustomer().contains(a);
+			
+			if(isAcceptable){
+				activity = a;
+				break;
+			}
+		}
+		
+		// Checks basic requirements
+		try{
+			Assert.notNull(activity, "No se ha encontrado una actividad con la que realizar la comprobación");
+		}catch (Exception e) {
+			throw new InvalidPreTestException(e.toString());
+		}
+		
+		// Execution of test
+		
+		authenticate("customer1");
+
+		activityService.cancel(activity);
+
+		// Checks results 
+		
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	@Transactional(noRollbackFor=IllegalArgumentException.class)
+	@Rollback(value=true)
+	public void testCancelActivityBookedErrorPast(){
+		Activity activity;
+		Customer custo;
+		Collection<Gym> gyms;
+		
+		// Load objects to test
+		authenticate("customer1");
+		
+		custo = customerService.findByPrincipal();
+		activity = null;
+		
+		gyms = gymService.findAllWithFeePaymentActive();
+		for(Gym gym:gyms){
+			Collection<Activity> activities;
+			activities = activityService.findAll();
+			for(Activity a:activities){
+				boolean isAcceptable;
+
+				isAcceptable = !a.getStartingMoment().after(new Date()) && ! a.getDeleted();
+				isAcceptable = isAcceptable && a.getCustomers().contains(custo);
+				isAcceptable = isAcceptable && gym.getRooms().contains(a.getRoom());
+			
+				if(isAcceptable){
+					activity = a;
+					break;
+				}
+			}
+		}
+		
+		// Checks basic requirements
+		try{
+			Assert.notNull(activity, "No se ha encontrado una actividad con la que realizar la comprobación");
+		}catch (Exception e) {
+			throw new InvalidPreTestException(e.toString());
+		}
+		
+		// Execution of test
+		
+		authenticate("customer1");
+
+		activityService.cancel(activity);
+
+		// Checks results 
+	}
+	
+	/**
+	 * Acme-Six-Pack-2.0 - Level B - 10.2.1
+	 * List the activities
+	 * 
+	 */
+	@Test
+	public void testListActivitiesAdminOk(){
+		Assert.notNull(null, "Método testListActivitiesAdminOk no realizado");
+		Activity activity;
+		
+		// Load objects to test
+		
+		activity = null;
+		
+		// Checks basic requirements
+		try{
+			Assert.notNull(activity, "No se ha encontrado una actividad con la que realizar la comprobación");
+		}catch (Exception e) {
+			throw new InvalidPreTestException(e.toString());
+		}
+		
+		// Execution of test
+
+		// Checks results 
+
+	}
+	
+	/**
+	 * Acme-Six-Pack-2.0 - Level B - 10.2.2
+	 * Create the activities
+	 * 
+	 */
+	@Test
+	public void testCreateActivitiesAdminOk(){
+		Activity activity;
+		Gym gym;
+		ServiceEntity service;
+		Trainer trainer;
+		Room room;
+		Date startingMoment;
+		
+		// Load objects to test
+		
+		authenticate("admin");
+		
+		activity = null;
+		gym = null;
+		service = null;
+		room = null;
+		trainer = null;
+		
+		startingMoment = new Date();
+		
+		Calendar c = Calendar.getInstance();
+		c.setTime(new Date());
+		c.add(Calendar.MONTH, +6);
+		startingMoment.setTime(c.getTimeInMillis());
+		
+		for(Gym g:gymService.findAll()){
+			for(ServiceEntity s:serviceService.findAllByGym(g.getId())){
+				room = null;
+				trainer = null;
+
+				for(Room r:g.getRooms()){
+						room = r;
+				}
+				
+				for(Trainer t:trainerService.findAllByServiceId(s.getId())){
+					if(t.getActivities().size() == 0)
+						trainer = t;
+				}
+				if(room != null && trainer != null){
+					gym = g;
+					service = s;					
+					break;
+				}
+			}
+		}
+		
+		// Checks basic requirements
+		try{
+			Assert.notNull(gym, "No se ha encontrado un gym con la que realizar la comprobación");
+			Assert.notNull(service, "No se ha encontrado un service con la que realizar la comprobación");
+			Assert.notNull(trainer, "No se ha encontrado un trainer con la que realizar la comprobación");
+			Assert.notNull(room, "No se ha encontrado un room con la que realizar la comprobación");
+		}catch (Exception e) {
+			throw new InvalidPreTestException(e.toString());
+		}
+		
+		// Execution of test
+		
+		authenticate("admin");
+		
+		activity = activityService.createWithGym(gym.getId(), service.getId());
+
+		activity.setTrainer(trainer);
+		activity.setTitle("activity_test_");
+		activity.setNumberOfSeatsAvailable(room.getNumberOfSeats());
+		activity.setStartingMoment(startingMoment);
+		activity.setDuration(0.5);
+		activity.setDescription("Any description");
+		activity.setRoom(room);
+		
+		activityService.saveToEdit(activity);
+
+		// Checks results 
+
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	@Transactional(noRollbackFor=IllegalArgumentException.class)
+	@Rollback(value=true)
+	public void testCreateActivitiesAdminErrorSeats(){
+		Activity activity;
+		Gym gym;
+		ServiceEntity service;
+		Trainer trainer;
+		Room room;
+		Date startingMoment;
+		
+		// Load objects to test
+		
+		authenticate("admin");
+		
+		activity = null;
+		gym = null;
+		service = null;
+		room = null;
+		trainer = null;
+		
+		startingMoment = new Date();
+		
+		Calendar c = Calendar.getInstance();
+		c.setTime(new Date());
+		c.add(Calendar.MONTH, +6);
+		startingMoment.setTime(c.getTimeInMillis());
+		
+		for(Gym g:gymService.findAll()){
+			for(ServiceEntity s:serviceService.findAllByGym(g.getId())){
+				room = null;
+				trainer = null;
+
+				for(Room r:g.getRooms()){
+						room = r;
+				}
+				
+				for(Trainer t:trainerService.findAllByServiceId(s.getId())){
+					if(t.getActivities().size() == 0)
+						trainer = t;
+				}
+				if(room != null && trainer != null){
+					gym = g;
+					service = s;					
+					break;
+				}
+			}
+		}
+		
+		// Checks basic requirements
+		try{
+			Assert.notNull(gym, "No se ha encontrado un gym con la que realizar la comprobación");
+			Assert.notNull(service, "No se ha encontrado un service con la que realizar la comprobación");
+			Assert.notNull(trainer, "No se ha encontrado un trainer con la que realizar la comprobación");
+			Assert.notNull(room, "No se ha encontrado un room con la que realizar la comprobación");
+		}catch (Exception e) {
+			throw new InvalidPreTestException(e.toString());
+		}
+		
+		// Execution of test
+		
+		authenticate("admin");
+		
+		activity = activityService.createWithGym(gym.getId(), service.getId());
+
+		activity.setTrainer(trainer);
+		activity.setTitle("activity_test_");
+		activity.setNumberOfSeatsAvailable(room.getNumberOfSeats() + 1);
+		activity.setStartingMoment(startingMoment);
+		activity.setDuration(0.5);
+		activity.setDescription("Any description");
+		activity.setRoom(room);
+		
+		activityService.saveToEdit(activity);
+
+		// Checks results 
+
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	@Transactional(noRollbackFor=IllegalArgumentException.class)
+	@Rollback(value=true)
+	public void testCreateActivitiesAdminErrorDate(){
+		Activity activity;
+		Gym gym;
+		ServiceEntity service;
+		Trainer trainer;
+		Room room;
+		Date startingMoment;
+		
+		// Load objects to test
+		
+		authenticate("admin");
+		
+		activity = null;
+		gym = null;
+		service = null;
+		room = null;
+		trainer = null;
+		
+		startingMoment = new Date();
+		
+		Calendar c = Calendar.getInstance();
+		c.setTime(new Date());
+		c.add(Calendar.MONTH, -1);
+		startingMoment.setTime(c.getTimeInMillis());
+		
+		for(Gym g:gymService.findAll()){
+			for(ServiceEntity s:serviceService.findAllByGym(g.getId())){
+				room = null;
+				trainer = null;
+
+				for(Room r:g.getRooms()){
+						room = r;
+				}
+				
+				for(Trainer t:trainerService.findAllByServiceId(s.getId())){
+					if(t.getActivities().size() == 0)
+						trainer = t;
+				}
+				if(room != null && trainer != null){
+					gym = g;
+					service = s;					
+					break;
+				}
+			}
+		}
+		
+		// Checks basic requirements
+		try{
+			Assert.notNull(gym, "No se ha encontrado un gym con la que realizar la comprobación");
+			Assert.notNull(service, "No se ha encontrado un service con la que realizar la comprobación");
+			Assert.notNull(trainer, "No se ha encontrado un trainer con la que realizar la comprobación");
+			Assert.notNull(room, "No se ha encontrado un room con la que realizar la comprobación");
+		}catch (Exception e) {
+			throw new InvalidPreTestException(e.toString());
+		}
+		
+		// Execution of test
+		
+		authenticate("admin");
+		
+		activity = activityService.createWithGym(gym.getId(), service.getId());
+
+		activity.setTrainer(trainer);
+		activity.setTitle("activity_test_");
+		activity.setNumberOfSeatsAvailable(room.getNumberOfSeats());
+		activity.setStartingMoment(startingMoment);
+		activity.setDuration(0.5);
+		activity.setDescription("Any description");
+		activity.setRoom(room);
+		
+		activityService.saveToEdit(activity);
+
+		// Checks results 
+
+	}
+	
+	/**
+	 * Acme-Six-Pack-2.0 - Level B - 10.2.3
+	 * Modify the activities
+	 * 
+	 */
+	@Test
+	public void testModifyActivitiesAdminOkSeats(){
+		Activity activity;
+
+		// Load objects to test
+		
+		authenticate("admin");
+		
+		activity = null;
+
+		for(Activity a:activityService.findAll()){
+			boolean isAcceptable;
+			
+			isAcceptable = a.getStartingMoment().after(new Date()) && a.getRoom().getNumberOfSeats() != a.getNumberOfSeatsAvailable();
+			
+			if(isAcceptable){
+				activity = a;
+			}
+		}
+		// Checks basic requirements
+		try{
+			Assert.notNull(activity, "No se ha encontrado un activity con la que realizar la comprobación");
+		}catch (Exception e) {
+			throw new InvalidPreTestException(e.toString());
+		}
+		
+		// Execution of test
+		
+		authenticate("admin");
+		
+		activity.setNumberOfSeatsAvailable(activity.getRoom().getNumberOfSeats());
+		activityService.saveToEdit(activity);
+
+		// Checks results 
+
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	@Transactional(noRollbackFor=IllegalArgumentException.class)
+	@Rollback(value=true)
+	public void testModifyActivitiesAdminErrorSeatsOver(){
+		Activity activity;
+
+		// Load objects to test
+		
+		authenticate("admin");
+		
+		activity = null;
+
+		for(Activity a:activityService.findAll()){
+			boolean isAcceptable;
+			
+			isAcceptable = a.getStartingMoment().after(new Date()) && a.getRoom().getNumberOfSeats() != a.getNumberOfSeatsAvailable();
+			
+			if(isAcceptable){
+				activity = a;
+			}
+		}
+		// Checks basic requirements
+		try{
+			Assert.notNull(activity, "No se ha encontrado un activity con la que realizar la comprobación");
+		}catch (Exception e) {
+			throw new InvalidPreTestException(e.toString());
+		}
+		
+		// Execution of test
+		
+		authenticate("admin");
+		
+		activity.setNumberOfSeatsAvailable(activity.getRoom().getNumberOfSeats()+1);
+		activityService.saveToEdit(activity);
+
+		// Checks results 
+
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	@Transactional(noRollbackFor=IllegalArgumentException.class)
+	@Rollback(value=true)
+	public void testModifyActivitiesAdminErrorSeatsLower(){
+		Activity activity;
+
+		// Load objects to test
+		
+		authenticate("admin");
+		
+		activity = null;
+
+		for(Activity a:activityService.findAll()){
+			boolean isAcceptable;
+			
+			isAcceptable = a.getStartingMoment().after(new Date()) && a.getCustomers().size() > 1;
+			
+			if(isAcceptable){
+				activity = a;
+			}
+		}
+		// Checks basic requirements
+		try{
+			Assert.notNull(activity, "No se ha encontrado un activity con la que realizar la comprobación");
+		}catch (Exception e) {
+			throw new InvalidPreTestException(e.toString());
+		}
+		
+		// Execution of test
+		
+		authenticate("admin");
+		
+		activity.setNumberOfSeatsAvailable(activity.getCustomers().size()-1);
+		activityService.saveToEdit(activity);
+
+		// Checks results 
+
+	}
+	
+	/**
+	 * Acme-Six-Pack-2.0 - Level B - 10.2.4
+	 * Delete an activities
+	 * 
+	 */
+	@Test
+	public void testDeleteActivitiesAdminOk(){
+		Activity activity;
+
+		// Load objects to test
+		
+		authenticate("admin");
+		
+		activity = null;
+
+		for(Activity a:activityService.findAll()){
+			boolean isAcceptable;
+			
+			isAcceptable = a.getStartingMoment().after(new Date()) && a.getCustomers().size() < 1;
+			
+			if(isAcceptable){
+				activity = a;
+				break;
+			}
+		}
+		// Checks basic requirements
+		try{
+			Assert.notNull(activity, "No se ha encontrado un activity con la que realizar la comprobación");
+		}catch (Exception e) {
+			throw new InvalidPreTestException(e.toString());
+		}
+		
+		// Execution of test
+		
+		authenticate("admin");
+		
+		activityService.delete(activity);
+
+		// Checks results 
+
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	@Transactional(noRollbackFor=IllegalArgumentException.class)
+	@Rollback(value=true)
+	public void testDeleteActivitiesAdminErrorCustomers(){
+		Activity activity;
+
+		// Load objects to test
+		
+		authenticate("admin");
+		
+		activity = null;
+
+		for(Activity a:activityService.findAll()){
+			boolean isAcceptable;
+			
+			isAcceptable = a.getStartingMoment().after(new Date()) && a.getCustomers().size() > 0;
+			
+			if(isAcceptable){
+				activity = a;
+				break;
+			}
+		}
+		// Checks basic requirements
+		try{
+			Assert.notNull(activity, "No se ha encontrado un activity con la que realizar la comprobación");
+		}catch (Exception e) {
+			throw new InvalidPreTestException(e.toString());
+		}
+		
+		// Execution of test
+		
+		authenticate("admin");
+		
+		activityService.delete(activity);
+
+		// Checks results 
+
+	}
 	
 	// Ancillary Methods -----------------------------
 	/**
