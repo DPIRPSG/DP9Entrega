d674683de6b0008681eab97b718e7a6ad4632d40
migrodcab <miguel.rc95@gmail.com>
2016-04-04 16:33:44 +0200

    Solved Issue #119 y warnings quitados

--- WorkSpace/Acme-Six-Pack/src/main/java/controllers/TrainerController.java
+++ WorkSpace/Acme-Six-Pack/src/main/java/controllers/TrainerController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import services.ActorService;
 import services.ServiceService;
 import services.TrainerService;
 
@@ -29,9 +28,6 @@ public class TrainerController extends AbstractController {
 	
 	@Autowired
 	private ServiceService serviceService;
-	
-	@Autowired
-	private ActorService actorService;
 
 	// Constructors ----------------------------------------------------------
 
